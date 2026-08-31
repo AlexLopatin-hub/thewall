bf985b93166eb86a624938767fbc9d94303721bf
alex <alex.lopatin456@gmail.com>
2026-08-31 17:11:54 +0300

    frontend setup

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import api from '@/api/api'
+
+const posts = ref([])
+
+const fetchData = async () => {
+  try {
+    const response = await api.getPosts()
+    posts.value = response.data
+  } catch (err) {
+    err.value = "Ошибка при загрузке данных"
+    alert(err)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>Hello, world!</h1>
+  </div>
+
+  <div class="wall">
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1 {
+    text-align: left;
+  }
+}
+</style>
